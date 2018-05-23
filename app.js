0aa389cbb9d2664df71dee94d78a8fd5be7fb4b2
Bianca Hale <sashabtt@gmail.com>
2018-05-22 22:55:09 -0600

    added editejs

--- app.js
+++ app.js
@@ -28,9 +28,7 @@ app.use('/tennis', tennis);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+  next(createError(404));
  });
  
  // error handlers
--- routes/tennis.js
+++ routes/tennis.js
@@ -4,7 +4,11 @@ var Tennis = require('../models').Tennis;
 
 /* GET movie listings. */
 router.get('/', function(req, res) {
-  Tennis.all()
+  Tennis.all({
+    order: [
+      ['createdAt', 'ASC']
+    ]
+  })
     .then( function(tennis) {
       return res.render('tennis', { tennis: tennis });
   })
@@ -18,5 +22,23 @@ router.post('/', function(req, res) {
       res.redirect('/tennis');
   });
 });
+
+router.get(':id/edit', function(req, res){
+  Tennis.findById(req.params.id)
+  .then(function(tennis){
+    return res.render('edit', {tennis: tennis});
+    })
+  })
+
+  router.put('/:id', function(req, res) {
+    Tennis.update(
+      { title: req.body.title },
+      { where: { id: req.params.id } }
+    )
+    .then( function() {
+      return res.redirect('/tennis');
+    })
+  });
+
 
 module.exports = router;
